2ea0b128c97e14cfb1319b4b71b07f102612a95c
pratyush-kr <pratyush151198@gmail.com>
2021-07-27 00:04:09 +0530

    changed-name

--- Tech/Class1/add-2-nos.c
+++ Tech/Class1/add-2-nos.c
@@ -7,7 +7,7 @@ int main()
     int x = 5;
     int y = 7;
     float f = 7.421;
-    printf("%*f\n", 2, f);
+    //printf("%*f\n", 2, f);
     int z = printf("%*c%*c", x,' ', y,' ');
     printf("sum = %d\n", z);
     return 0;
