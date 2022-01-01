331d1dca8a6c2f6c2df70d7740a44989a6ed95ef
pratyush-kr <pratyush151198@gmail.com>
2022-01-02 00:45:27 +0530

    134

--- Assignments/A3/half-sort.cpp
+++ Assignments/A3/half-sort.cpp
@@ -1,12 +1,38 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
 
-void merge_sort(vector<int> &arr, int l, int r);
-void sort(vector<int> &arr, int n);
-void merge(vector<int> &arr, const int l, const int m, const int r);
-int middle;
+void Heapify(vector<int> &arr, int i, int size, auto comp)
+{
+    int tmp = i;
+    int l = 2*i+1;
+    int r = 2*i+2;
+    tmp = (l<size && comp(arr[tmp], arr[l]))? l:tmp;
+    tmp = (r<size && comp(arr[tmp], arr[r]))? r:tmp;
+    if(tmp != i)
+    {
+        swap(arr[tmp], arr[i]);
+        Heapify(arr, tmp, size, comp);
+    }
+}
+
+void buildHeap(vector<int> &arr, int l, int r, auto comp)
+{
+    int size = r+1;
+    int startIdx = (r+l)/2 - 1;
+    for(int i=startIdx; i>=l; i--)
+        Heapify(arr, i, size, comp);
+}
+
+void sort(vector<int> &arr, int l, int r, auto comp)
+{
+    buildHeap(arr, l, r, comp);
+    for(int i=r; i>=l; i--)
+    {
+        swap(arr[l], arr[i]);
+        Heapify(arr, l, i, comp);
+    }
+}
 
 void print(vector<int> arr)
 {
@@ -20,62 +46,24 @@ void print(vector<int> arr)
 int main()
 {
     vector<int> arr;
-    int array[] = {5, 4, 6, 2, 1, 3, 8, 9, 7};
+    int array[] = {5, 4, 6, 2, 1, 3, 8, 9, 7, 0};
     int n = sizeof(array)/sizeof(*array);
-    middle = n/2;
+    int middle = n/2;
     for(int i=0; i<n; i++)
         arr.push_back(array[i]);
-    merge_sort(arr, 0, n-1);
+    sort(arr, 0, middle-1, [](int a, int b){
+        return (a < b);
+    });
+    vector<int> arr2;
+    for(int i=middle; i<n; i++)
+        arr2.push_back(array[i]);
+    sort(arr2, 0, arr2.size()-1, [](int a, int b){
+        return (a > b);
+    });
+    for(int i=middle; i<n; i++)
+        arr[i] = arr2[i];
     for(int i=0; i<n; i++)
         cout<<arr[i]<<' ';
     cout<<'\n';
     return 0;
-}
-
-void merge_sort(vector<int> &arr, int l, int r)
-{
-    if(l < r)
-    {
-        int m = (l+r)/2;
-        merge_sort(arr, l, m);
-        merge_sort(arr, m+1, r);
-        merge(arr, l, m, r);
-    }
-}
-
-void merge(vector<int> &arr, int l, int m, int r)
-{
-    vector<int> array;
-    int i=l, j=m+1;
-    while(i<=m && j<=r)
-    {
-        if(l <= middle)
-        {
-            if(arr[i] < arr[j])
-                array.push_back(arr[i++]);
-            else
-                array.push_back(arr[j++]);
-        }
-        else
-        {
-            if(arr[i] > arr[j])
-                array.push_back(arr[i++]);
-            else
-                array.push_back(arr[j++]);
-        }
-    }
-    while(i <= m)
-    {
-        array.push_back(arr[i]);
-        i++;
-    }
-    while(j <= r)
-    {
-        array.push_back(arr[j]);
-        j++;
-    }
-    for(int i=l; i<=r; i++)
-    {
-        arr[i] = array[i-l];
-    }
 }
